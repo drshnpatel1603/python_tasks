c2cee6df141d363c1aacfc4eb8dfbf812775613f
drshnpatel1603 <drshnmalli@gmail.com>
2025-02-20 16:38:12 +0530

    [ADD] function file

---
+++ function.py
@@ -0,0 +1,28 @@
+''' with parameter positinal parameter '''
+# def sum(a,b,c):
+#     return a+b+c
+#
+# total = sum(10,20,30)
+# print(total)
+
+""" with keyword argument parameter """
+# def addition(a,b,c):
+#     return a+b+c
+#
+# total = addition(c=10,b=20,a=30)
+# print(total)
+
+''' *args argument '''
+# def addition(*args):
+#     return args[0], args[1]
+#
+# total1, total2 = addition(10,20,30)
+# print(total1, total2)
+
+""" **kwargs argumnet : in give to argument - key=value pair """
+# def show_dict(**kwargs):
+#     print(f"name is : {kwargs['name']} & age is : {kwargs['age']}")
+#
+# show_dict(name="drshn", age=22)
+
+
