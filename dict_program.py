562761137d4dbb0f321c38de000c9fddda85327f
drshnpatel1603 <drshnmalli@gmail.com>
2025-02-21 15:12:50 +0530

    [ADD] dictinary program

---
+++ dict_program.py
@@ -0,0 +1,81 @@
+sales_data = {
+    "Store_A": {
+        "Laptop": 15,
+        "Phone": 30,
+        "Tablet": 10
+    },
+    "Store_B": {
+        "Laptop": 25,
+        "Phone": 20,
+        "Tablet": 15
+    },
+    "Store_C": {
+        "Laptop": 10,
+        "Phone": 35,
+        "Tablet": 5
+    }
+}
+
+def space(fx):
+    def wapper(*args,**kwargs):
+        print("\n")
+        result = fx(*args,**kwargs)
+        return result
+    return wapper
+
+""" Find the total sales for each product across all stores. """
+# @space
+def total_sells(sales_data):
+    sale_each_produce = {}
+    for value in sales_data.values():
+        for key,val in value.items():
+            if sale_each_produce.get(key):
+                sale_each_produce[key] += val
+            else:
+                sale_each_produce.update({key : val})
+    return sale_each_produce
+total_sell = total_sells(sales_data)
+print(f"Total Price each Product : {total_sell}")
+
+
+""" Identify the store with the highest total sales (sum of all products). """
+@space
+def highest_sales(sales_data):
+    high_sale = {}
+    for key,value in sales_data.items():
+        total = 0
+        for val in value.values():
+            total += val
+        high_sale.update({key : total})
+
+    high_selling_store = max([(value, key) for key, value in high_sale.items()])
+
+    return high_selling_store
+
+print(f"highest sale store wise : {highest_sales(sales_data)}")
+
+""" Find the best-selling product (i.e., the product with the highest total sales). """
+@space
+def find_best_product(total_sells):
+    k = ""
+    max = 0
+    for key,val in total_sells.items():
+        if val > max:
+            max = val
+            k = key
+    return (k,max)
+print(f"highset selling product : {find_best_product(total_sell)}")
+
+""" Sort the stores based on their total sales in descending order and print the sorted result. """
+@space
+def high_sale_store(sales_data):
+    list_store = {}
+    for key,value in sales_data.items():
+        total = 0
+        for val in value.values():
+            total += val
+        list_store.update({key:total})
+    list_store = ({k : v for k,v in sorted(list_store.items(), key=lambda key: key[1], reverse=True)})
+    return list_store
+
+print(f"store wise List : {high_sale_store(sales_data)}")
